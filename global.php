8d897065012834824a3a8839615f6ccf8033d686
NayelliSZ <nayellisotoz@gmail.com>
2022-11-26 10:12:16 -0600

    Back
    api_ticket

---
+++ global.php
@@ -0,0 +1,14 @@
+<?php
+//Definir Servidor
+define("DB_HOST", "v2ray.dosedgames.tk");
+
+//Define nombre de BD
+define("DB_NAME","helpdesk");
+
+//usuario bd 
+define("DB_USERNAME","Pichardo");
+
+//password
+define("DB_PASSWORD","pichardo121289");
+
+?>
